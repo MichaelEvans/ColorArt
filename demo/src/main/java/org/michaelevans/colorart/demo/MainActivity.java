c17a5e89ce82f1d6b549144d62a9fddda2196804
Michael Evans <michaelcevans@me.com>
2013-12-03 16:00:57 -0500

    introduce fadedimageview
    move shader allocations to onSizeChanged
    
    add setting to toggle fading
    
    support multiple orientations for fading

--- demo/src/main/java/org/michaelevans/colorart/demo/MainActivity.java
+++ demo/src/main/java/org/michaelevans/colorart/demo/MainActivity.java
@@ -43,26 +43,6 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Intent i = new Intent(MainActivity.this, DetailsActivity.class);
         i.putExtra("position", position);
